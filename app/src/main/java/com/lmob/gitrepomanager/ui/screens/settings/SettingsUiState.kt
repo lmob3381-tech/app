09e64350b97e9051e7b0d86469d570cf32106cd7
lmob3381-tech <lmob3381@gmail.com>
2026-07-21 16:46:28 +0700

    chore: kosongkan isi repo

--- app/src/main/java/com/lmob/gitrepomanager/ui/screens/settings/SettingsUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.lmob.gitrepomanager.ui.screens.settings
-
-import com.lmob.gitrepomanager.data.model.GitHubUser
-
-data class SettingsUiState(
-    val user: GitHubUser? = null,
-    val isLoading: Boolean = false,
-    val didLogout: Boolean = false
-)
